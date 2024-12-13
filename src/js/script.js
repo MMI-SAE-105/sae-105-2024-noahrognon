46009339c565d54a03847b36606f263c528cef51
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2024-12-13 11:10:23 +0100

    Add "scroll to top" button and related styles and functionality

--- src/js/script.js
+++ src/js/script.js
@@ -25,4 +25,19 @@ nextButton.addEventListener('click', () => {
         behavior: 'smooth',
     });
 });
+const scrollToTopButton = document.getElementById("scrollToTop");
 
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+        scrollToTopButton.style.display = "flex";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
